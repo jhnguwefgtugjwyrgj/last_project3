a2b5fbda36375c3676f3c93bcc6b64fb69f6a387
jhnguwefgtugjwyrgj <in9.var@yandex.ru>
2020-05-12 18:44:27 +0300

    Initial commit

---
+++ flask4/data/users.py
@@ -0,0 +1,29 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    buys = sqlalchemy.Column(sqlalchemy.String, default='')
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    news = orm.relation('News', back_populates='user')
+    all_price = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return f'{self.id}, {self.name}, {self.about}, {self.email}'
